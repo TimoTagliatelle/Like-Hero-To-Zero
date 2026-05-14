08e435b70916c9823af164826d3ae7a8a50b580b
Timo Tagliatelle <Lakess9@gmx.net>
2026-05-14 15:10:02 +0200

    Initial Spring Boot setup

---
+++ src/test/java/com/likehero/likeherotozero/LikeherotozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likehero.likeherotozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeherotozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
