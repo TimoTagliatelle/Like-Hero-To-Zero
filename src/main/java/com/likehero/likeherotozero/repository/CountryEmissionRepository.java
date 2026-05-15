583f981fc0bafd60812e79e2cc79e00d8777703d
Timo Tagliatelle <Lakess9@gmx.net>
2026-05-15 09:37:13 +0200

    4rw

--- src/main/java/com/likehero/likeherotozero/repository/CountryEmissionRepository.java
+++ src/main/java/com/likehero/likeherotozero/repository/CountryEmissionRepository.java
@@ -3,5 +3,9 @@ package com.likehero.likeherotozero.repository;
 import com.likehero.likeherotozero.model.CountryEmission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CountryEmissionRepository extends JpaRepository<CountryEmission, Long> {
+
+   List<CountryEmission> findByCountry(String country);
 }
