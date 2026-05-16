cdf6727e73357581083c375f917cd51d3ec726f2
Timo Tagliatelle <Lakess9@gmx.net>
2026-05-16 09:40:33 +0200

    dasa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO country_emission (country, co2_emission2022, co2_emission2023, approved)
-VALUES ('Germany', 750.5, 720.3, true);
+VALUES ('Germany', 750.5, 720.3, 1);
 
 INSERT INTO country_emission (country, co2_emission2022, co2_emission2023, approved)
-VALUES ('France', 420.1, 400.0, true);
+VALUES ('France', 420.1, 400.0, 1);
 
 INSERT INTO country_emission (country, co2_emission2022, co2_emission2023, approved)
-VALUES ('USA', 4854, 4680, true);
+VALUES ('USA', 4854, 4680, 1);
