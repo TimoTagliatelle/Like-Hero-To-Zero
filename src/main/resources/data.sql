cacc1ea3fa7ca0bf6d39414c1a4a5195efba8e14
Timo Tagliatelle <Lakess9@gmx.net>
2026-05-16 08:47:41 +0200

    gdgege

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO country_emission (country, co2_emission2022, co2_emission2023)
-VALUES ('Germany', 750.5, 720.3);
+INSERT INTO country_emission (country, co2_emission2022, co2_emission2023, approved)
+VALUES ('Germany', 750.5, 720.3, true);
 
-INSERT INTO country_emission (country, co2_emission2022, co2_emission2023)
-VALUES ('France', 420.1, 400.0);
+INSERT INTO country_emission (country, co2_emission2022, co2_emission2023, approved)
+VALUES ('France', 420.1, 400.0, true);
 
-INSERT INTO country_emission (country, co2_emission2022, co2_emission2023)
-VALUES ('USA', 4854, 4680);
+INSERT INTO country_emission (country, co2_emission2022, co2_emission2023, approved)
+VALUES ('USA', 4854, 4680, true);
